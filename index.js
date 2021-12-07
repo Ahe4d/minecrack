57ed4e9f5a9be5c216e49d5694c879db191fd85e
Etienne Ghashehbaba <eghashehbaba@gmail.com>
2021-12-07 04:58:05 -0800

    How do i develop

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.set('view engine', 'ejs');
 
 app.use("/", express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 8080, function(srv) {
-  console.log('Application up at ' + srv.port);
+let listener = app.listen(process.env.PORT || 8080, function(srv) {
+  console.log('Application up at port ' + listener.address().port);
 })
